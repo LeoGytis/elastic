02d1931177247b82f93d48828b7bd6b8372cb392
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-05-22 11:24:51 +0300

    save

--- src/components/sections/SectionThree/Doctors.tsx
+++ src/components/sections/SectionThree/Doctors.tsx
@@ -5,31 +5,29 @@ import {doctorsData} from './doctorsData';
 const Doctors = () => {
 	return (
 		<article className="flex flex-col items-center ">
-			<h3>Gydytojai</h3>
-			<h1 className="pb-10">Komanda, kuri įsiklauso</h1>
+			<h3 className="mb-6">Gydytojai</h3>
+			<h1 className="mb-10">Komanda, kuri įsiklauso</h1>
 
-			<div className="w-[95dvw] lg:w-[98dvw] border-4 max-w-full border-blue-500">
-				<Slider autoSlideInterval={3000}>
-					{doctorsData.map((doctor, index) => (
-						<div key={index} className="px-4">
-							<img
-								src={doctor.image}
-								alt={doctor.alt}
-								className="w-full aspect-[9/12] object-cover mb-4"
-							/>
-							<div className="mb-6">
-								<p className="text-2xl">{doctor.fullName}</p>
-								<div className="font-lato font-normal leading-[160%] tracking-[0.01em] text-white/60">
-									{doctor.speciality}
-								</div>
+			<Slider autoSlideInterval={2500}>
+				{doctorsData.map((doctor, index) => (
+					<div key={index} className="px-4 max-lg:text-center">
+						<img
+							src={doctor.image}
+							alt={doctor.alt}
+							className="w-1/2 lg:w-full aspect-[9/12] object-cover mb-4 mx-auto"
+						/>
+						<div className="mb-6">
+							<p className="text-2xl">{doctor.fullName}</p>
+							<div className="font-lato font-normal leading-[160%] tracking-[0.01em] text-white/60">
+								{doctor.speciality}
 							</div>
-							<Button variant="dark" className="p-0">
-								Plačiau
-							</Button>
 						</div>
-					))}
-				</Slider>
-			</div>
+						<Button variant="dark" className="p-0">
+							Plačiau
+						</Button>
+					</div>
+				))}
+			</Slider>
 
 			<Button variant="light" className="my-8 lg:mt-28">
 				Visi gydytojai
--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -1,17 +1,17 @@
+import {useMediaQuery} from '@/hooks/useMediaQuery';
 import React, {useCallback, useEffect, useState} from 'react';
 
 interface SliderProps {
 	children: React.ReactNode;
-	className?: string;
 	autoSlideInterval?: number;
 }
 
 export const Slider: React.FC<SliderProps> = ({
 	children,
-	className = '',
 	autoSlideInterval = 3000,
 }) => {
 	const [currentIndex, setCurrentIndex] = useState(0);
+	const isDesktop = useMediaQuery('(min-width: 1024px)');
 	const childrenArray = React.Children.toArray(children);
 
 	const duplicatedChildren = [
@@ -48,20 +48,29 @@ export const Slider: React.FC<SliderProps> = ({
 		}
 	};
 
+	const transformStyle = {
+		transform: isDesktop
+			? `translateX(-${(currentIndex * 100) / 5}%)`
+			: `translateX(-${currentIndex * 100}%)`,
+	};
+
 	return (
-		<div className={`relative w-full overflow-hidden ${className}`}>
-			<div
-				className="flex transition-transform duration-1000 ease-in-out"
-				style={{
-					transform: `translateX(-${currentIndex * 100}%)`,
-				}}
-				onTransitionEnd={handleTransitionEnd}
-			>
-				{duplicatedChildren.map((child, index) => (
-					<div key={index} className="flex-shrink-0 w-full md:w-1/5">
-						{child}
-					</div>
-				))}
+		<div className="w-[95dvw] lg:w-[98dvw]">
+			<div className="overflow-hidden">
+				<div
+					className="flex transition-transform duration-1000 ease-in-out"
+					style={transformStyle}
+					onTransitionEnd={handleTransitionEnd}
+				>
+					{duplicatedChildren.map((child, index) => (
+						<div
+							key={index}
+							className="flex-shrink-0 w-full lg:w-1/5"
+						>
+							{child}
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
